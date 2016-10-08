07cd60aa959647c4a55048cfa1f58e8799c92c0a
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-08 15:59:24 +0200

    02 zmiana
    Zmiana 02 próba zatwierdzenia

--- password_hash.php
+++ password_hash.php
@@ -1,5 +1,7 @@
 <?php
-echo password_hash("1234", PASSWORD_BCRYPT)."\n";
+
+$input = 'ala ma kota';
+echo password_hash($input, PASSWORD_BCRYPT)."\n";
 
 
 $hash = '$2y$10$Llme1nljLV2c6qmvyNj6y.Eov2J10.BMdQSUc6SI7i2tLJVXl49Nu';
